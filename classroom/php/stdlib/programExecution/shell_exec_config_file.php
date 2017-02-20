54ab51f6a04ac816cd2047349f21f54683a5f145
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2017-02-20 18:11:07 -0300

    update Vagrantfile

--- classroom/php/stdlib/programExecution/shell_exec_config_file.php
+++ classroom/php/stdlib/programExecution/shell_exec_config_file.php
@@ -30,3 +30,9 @@
 // /var/named/mydomain_name.db
 // ~/.bash_aliases
 // ~/.bashrc
+
+shell_exec("crontab <<EOF
+00 09 * * 1-5 echo hello
+EOF");
+
+echo shell_exec("crontab -l");
--- classroom/php/stdlib/programExecution/shell_exec_root.php
+++ classroom/php/stdlib/programExecution/shell_exec_root.php
@@ -1,22 +1,26 @@
 <?php
 
+// shell_exec root
+echo shell_exec('sudo cat /etc/shadow');
+
+// SSH
 $connection = ssh2_connect('localhost', 22);
-ssh2_auth_password($connection, 'vagrant', 'vagrant');
+ssh2_auth_password($connection, 'ubuntu', 'ubuntu');
 
 $stream = ssh2_exec($connection, 'sudo cat /etc/shadow');
 stream_set_blocking($stream, true);
 $stream_out = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO);
 $output = stream_get_contents($stream_out);
+echo "<pre>{$output}</pre>";
 
+// http://www.tldp.org/LDP/lame/LAME/linux-admin-made-easy/shadow-file-formats.html
 // https://www.aychedee.com/2012/03/14/etc_shadow-password-hash-formats/
 // https://www.cyberciti.biz/faq/understanding-etcshadow-file/
+// http://php.net/manual/en/function.crypt.php
 // 1 -> MD5
 // 2a -> Blowfish
 // 5 -> SHA-256
 // 6 -> SHA-512
 // mkpasswd -m sha-512 // sudo apt-get install whois
 // mkpasswd -m sha-512 -S H.czqcYc vagrant
-echo "<pre>{$output}</pre>";
-
-http://php.net/manual/en/function.crypt.php
-echo crypt('vagrant', '$6$H.czqcYc$');
+// echo crypt('ubuntu', '$6$4HVs4t9N$');
